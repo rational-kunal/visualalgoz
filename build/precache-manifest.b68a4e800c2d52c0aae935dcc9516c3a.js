5de2b16f3e724288bef418c6711b4942cd4c2701
rational <kunalgk1999@gmail.com>
2020-01-26 18:25:59 +0530

    deployment

---
+++ build/precache-manifest.b68a4e800c2d52c0aae935dcc9516c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339fc4ecccd9561a4568585bb93e0042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2e5e6b6a0bb6a078ea",
+    "url": "/static/css/main.23cbcd1a.chunk.css"
+  },
+  {
+    "revision": "e5979dc26677d52b532b",
+    "url": "/static/js/2.7cd539ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7cd539ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b2e5e6b6a0bb6a078ea",
+    "url": "/static/js/main.0b60a4b7.chunk.js"
+  },
+  {
+    "revision": "6c77b3e6d4a655bbc678",
+    "url": "/static/js/runtime-main.7d0b1c2d.js"
+  }
+]);
